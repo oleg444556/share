8c9d52ae533d75b829d677d9cad80e463c9f50aa
The_Pimp <mishanin_oleg@bk.ru>
2023-10-11 00:33:38 +0300

    2 дня фиксил то, что забыл поставить один /

--- lyceum/catalog/tests.py
+++ lyceum/catalog/tests.py
@@ -9,5 +9,5 @@ class StaticUrlTests(TestCase):
 
     def test_catalog_item_detail_endpoint(self):
         # test for catalog item details
-        response = Client().get("/catalog/1")
+        response = Client().get("/catalog/1/")
         self.assertEqual(response.status_code, 200)

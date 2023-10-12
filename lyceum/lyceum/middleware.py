c9721262dbe81e11dab21f27bcc7e2e5ff640d87
The_Pimp <mishanin_oleg@bk.ru>
2023-10-12 22:47:40 +0300

    Подправил middleware

--- lyceum/lyceum/middleware.py
+++ lyceum/lyceum/middleware.py
@@ -7,9 +7,9 @@ class RussianWordsReverseMiddleware:
     def __init__(self, get_response):
         self._get_response = get_response
 
-    def __call__(self, request, *args, **kwargs):
+    def __call__(self, request):
         RussianWordsReverseMiddleware._call_count += 1
-        response = self._get_response(request, *args, **kwargs)
+        response = self._get_response(request)
         allowreverse = settings.ALLOW_REVERSE
         if self._call_count % 10 == 0 and allowreverse:
             text = response.content.decode("utf-8")

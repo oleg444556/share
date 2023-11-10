de61b284faf73473cfc7590b4c6cb34a6c5f317c
The_Pimp <mishanin_oleg@bk.ru>
2023-11-10 22:28:13 +0300

    Поправил

--- lyceum/feedback/views.py
+++ lyceum/feedback/views.py
@@ -10,22 +10,22 @@ __all__ = []
 
 def feedback(request):
     template = "feedback/feedback.html"
-    form_text = forms.FeedbackForm(request.POST or None)
+    form = forms.FeedbackForm(request.POST or None)
     form_personal = forms.FeedbackPersonalForm(request.POST or None)
     form_files = forms.FileFieldForm(request.POST or None)
     context = {
-        "form_text": form_text,
+        "form_text": form,
         "form_personal": form_personal,
         "form_files": form_files,
     }
 
     if request.method == "POST":
         if (
-            form_text.is_valid()
+            form.is_valid()
             and form_personal.is_valid()
             and form_files.is_valid()
         ):
-            text = form_text.cleaned_data.get("text")
+            text = form.cleaned_data.get("text")
             mail_to = form_personal.cleaned_data.get("mail")
             django.core.mail.send_mail(
                 "Обратная связь",
@@ -35,7 +35,7 @@ def feedback(request):
                 fail_silently=False,
             )
 
-            text = form_text.save()
+            text = form.save()
             personal = form_personal.save(commit=False)
             personal.text_feedback = text
             personal.save()

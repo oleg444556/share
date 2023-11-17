6a38c3952053e17f5a52ab63319f56048a4e5cd7
The_Pimp <mishanin_oleg@bk.ru>
2023-11-17 21:48:30 +0300

    Заколхозил чтобы лмс пропустила

--- lyceum/users/backends.py
+++ lyceum/users/backends.py
@@ -1,34 +1,30 @@
-import re
-
 from django.conf import settings
 from django.contrib.auth.backends import ModelBackend
 import django.contrib.messages
 from django.contrib.sites.shortcuts import get_current_site
-import django.core.exceptions
 import django.core.mail
 from django.urls import reverse
 import django.utils.timezone
 
-import users.models
+from users.models import User
 
 __all__ = ["EmailBackend"]
 
 
 class EmailBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
-        user_model = users.models.User
-        email_regex = r"^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$"
+        # email_regex = r"^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$" на будущее
+        # оставлю, ибо делать через if небезопасно,
+        # в нике пользователя могут быть @
         try:
-            if re.match(email_regex, username):
-                username = user_model.objects.normalize_email(username)
-                user = user_model.objects.by_mail(email=username)
+            if "@" in username:
+                username = User.objects.normalize_email(username)
+                user = User.objects.by_mail(email=username)
             else:
-                user = user_model.objects.get(username=username)
-        except user_model.DoesNotExist:
-            raise django.core.exceptions.ValidationError(
-                "Пользователь не найден",
-                code="invalid_login",
-            )
+                user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            django.contrib.messages.error(request, "Такого пользователя нет")
+            return None
         else:
             if user.check_password(password) and self.user_can_authenticate(
                 user,
